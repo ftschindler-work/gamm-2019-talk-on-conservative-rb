8d6f437fad6f8829bfe3b6bac178e8dc0c824c6d
Felix Schindler <felix.schindler@wwu.de>
2019-09-26 11:04:13 +0200

    update environments

--- environments/debian-full/PATH.sh
+++ environments/debian-full/PATH.sh
@@ -1,9 +1,19 @@
 DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ;  pwd -P )"
 export BASEDIR=$(cd ${DIR}/../.. && pwd)
-export INSTALL_PREFIX=${DIR}/local
+export INSTALL_PREFIX=${DIR}
 export PATH=${INSTALL_PREFIX}/bin:$PATH
 export LD_LIBRARY_PATH=${INSTALL_PREFIX}/lib64:${INSTALL_PREFIX}/lib:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=${INSTALL_PREFIX}/lib64/pkgconfig:${INSTALL_PREFIX}/lib/pkgconfig:${INSTALL_PREFIX}/share/pkgconfig:$PKG_CONFIG_PATH
-export F77=gfortran
-export CMAKE_FLAGS="-DCMAKE_PREFIX_PATH=$INSTALL_PREFIX"
-export OMP_NUM_THREADS=1
+export CMAKE_FLAGS="-DDUNE_PYTHON_VIRTUALENV_EXECUTABLE=python3 -DDUNE_PYTHON_VIRTUALENV_SETUP=FALSE -DDUNE_PYTHON_ALLOW_GET_PIP=FALSE"
+export SIMDB_GIT_REPOS=$BASEDIR
+export SIMDB_PATH=${BASEDIR}/DATA
+if [ "X${OPTS}" == "X" ]; then
+    echo "You did not define OPTS (export OPTS=gcc-relwithdebinfo), not loading a virtualenv!"
+else
+    if [ -e "${DIR}/venv/dune-${OPTS}/bin/activate" ]; then
+        source "${DIR}/venv/dune-${OPTS}/bin/activate"
+    else
+        echo "WARNING: missing virtualenv for OPTS=${OPTS}, did you call setup.sh?"
+    fi
+fi
+
